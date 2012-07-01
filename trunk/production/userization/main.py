47aafaf980f4fa506b90041d780523dd7833543f
jhkoivis <jhkoivis@f08f248d-ffcc-2425-270f-e839c19a9a44>
2012-07-01 19:19:38 +0000

    added shift+windows+3 = printscreen added main.py

---
+++ trunk/production/userization/main.py
@@ -0,0 +1,15 @@
+
+#if system == windows:
+#    if computer == macbook:
+#        add_autohotkeyMacbook_to_startup:
+#    else:
+#        add_autohotkey_to_startup:
+#
+#if system == darwin:
+#    backup_bashrc
+#    replace_basrch
+#    
+#if system == linux:
+#    backup_bashrc
+#    replace_basrch
+#    
