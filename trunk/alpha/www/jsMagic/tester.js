da52f8c37ea698f49e020b353656a579133250e6
jhkoivis <jhkoivis@f08f248d-ffcc-2425-270f-e839c19a9a44>
2013-02-23 16:21:05 +0000

    added more functionality...

---
+++ trunk/alpha/www/jsMagic/tester.js
@@ -0,0 +1,14 @@
+
+tester = {
+		
+		loadTestText: function(i) {
+			var id = sprintf(sprintf('%010d_task_textarea', i));
+			//var text = document.getElementById(id).val();
+			//return text;
+		},
+		
+		loadSolutionText: function() {
+			
+		}
+		
+}
