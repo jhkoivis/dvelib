f80508a28b7f6958b8a4d1ce468994a00477e9f6
jhkoivis <jhkoivis@f08f248d-ffcc-2425-270f-e839c19a9a44>
2013-02-24 10:21:21 +0000

    added working test.html

--- trunk/alpha/www/jsMagic/tester.js
+++ trunk/alpha/www/jsMagic/tester.js
@@ -1,14 +1,40 @@
 
 tester = {
 		
-		loadTestText: function(i) {
-			var id = sprintf(sprintf('%010d_task_textarea', i));
-			//var text = document.getElementById(id).val();
-			//return text;
+		loadTextById: function(id) {
+			var jqueryId = '#' + id;
+			var data = $(jqueryId).val();
+			return data;
 		},
 		
-		loadSolutionText: function() {
-			
-		}
+		loadSolutionTextById: function(id) {
+			// DEPRECATED, use loadTextById instead
+			return tester.loadTextById(id);
+		},
 		
+		evalText: function(text){
+			//http://stackoverflow.com/questions/1271516/executing-anonymous-functions-created-using-javascript-eval
+			var a = new Function('arg1', 'arg2', text); 
+			return a;
+		},
+		
+		runTest: function(solutionTextAreaId, testTextAreaId){
+			var solutionText = tester.loadTextById(solutionTextAreaId);
+			// no var infront of this: has to be global
+			arg1 = '1';
+			solution = tester.evalText(solutionText, arg1); 
+			
+			var testText = tester.loadTextById(testTextAreaId);
+			// no var infront of this: has to be global
+			test = tester.evalText(testText);
+			
+
+			return test();
+		},
+		
+		runTestByIndex: function(i){
+			solutionId = sprintf('%010d_solution_textarea', i);
+			testId = sprintf('%010d_task_textarea', i);
+			return tester.runTest(solutionId, testId);
+		}
 }
