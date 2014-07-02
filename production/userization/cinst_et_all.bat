4b6d84fcb877a144e47b7e34a5c8360fa0eeeb11
jhkoivis@gmail.com <jhkoivis@gmail.com@f08f248d-ffcc-2425-270f-e839c19a9a44>
2014-07-02 01:34:16 +0000

    initial

---
+++ production/userization/cinst_et_all.bat
@@ -0,0 +1,8 @@
+
+#############################
+# get this file             #
+#############################
+# @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
