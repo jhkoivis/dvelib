fc59af71646cdc7420e23c2d74587203b8d446eb
jhkoivis@gmail.com <jhkoivis@gmail.com@f08f248d-ffcc-2425-270f-e839c19a9a44>
2014-07-02 01:40:30 +0000

    fixed command

--- trunk/production/userization/cinst_et_all.bat
+++ trunk/production/userization/cinst_et_all.bat
@@ -3,5 +3,5 @@
 #############################
 # @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://dvelib.googlecode.com/svn/trunk/production/userization/cinst_et_all.bat'))"
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
